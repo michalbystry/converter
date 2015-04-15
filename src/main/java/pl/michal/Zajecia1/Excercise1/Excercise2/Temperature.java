6ebec396196e8e9b28c92be4357b1878f99d070d
michalbystry <michalbystry>
2015-04-15 22:30:11 +0200

    comit temperature

--- src/main/java/pl/michal/Zajecia1/Excercise1/Excercise2/Temperature.java
+++ src/main/java/pl/michal/Zajecia1/Excercise1/Excercise2/Temperature.java
@@ -1,37 +1,37 @@
-package pl.michal.Zajecia1.Excercise1.Excercise2;
-
-/**
- * Created by Michał on 2014-10-23.
- */
-public class Temperature {
-    private String temperatureSyntax;
-    private double temperatureValue;
-
-
-    public void setTemperatureSyntax(String temperatureSyntax) {
-        this.temperatureSyntax = temperatureSyntax;
-    }
-
-    public void setTemperatureValue(double temperatureValue) {
-        this.temperatureValue = temperatureValue;
-    }
-
-    public double getTemperatureValue() {
-        return temperatureValue;
-    }
-
-    public String getTemperatureSyntax() {
-        return temperatureSyntax;
-    }
-
-//    public void printTemp() {
-//        if (true == isCorrectValue) {
-//            System.out.println(getTemperatureValue() + " " + getTemperatureSyntax().toUpperCase());
-//        }
+//package pl.michal.Zajecia1.Excercise1.Excercise2;
+//
+///**
+// * Created by Michał on 2014-10-23.
+// */
+//public class Temperature {
+//    private String temperatureSyntax;
+//    private double temperatureValue;
+//
+//
+//    public void setTemperatureSyntax(String temperatureSyntax) {
+//        this.temperatureSyntax = temperatureSyntax;
 //    }
-
-
-}
-
-
-
+//
+//    public void setTemperatureValue(double temperatureValue) {
+//        this.temperatureValue = temperatureValue;
+//    }
+//
+//    public double getTemperatureValue() {
+//        return temperatureValue;
+//    }
+//
+//    public String getTemperatureSyntax() {
+//        return temperatureSyntax;
+//    }
+//
+////    public void printTemp() {
+////        if (true == isCorrectValue) {
+////            System.out.println(getTemperatureValue() + " " + getTemperatureSyntax().toUpperCase());
+////        }
+////    }
+//
+//
+//}
+//
+//
+//
